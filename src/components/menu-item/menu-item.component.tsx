fb1b3babefb8dffc29c884c60e4fc34393b9c916
Rob Clark <rob.clark@outlook.com>
2020-12-11 10:55:59 -0700

    added router-dom

--- src/components/menu-item/menu-item.component.tsx
+++ src/components/menu-item/menu-item.component.tsx
@@ -1,14 +1,35 @@
 import React from "react";
+import { withRouter, RouteComponentProps } from "react-router-dom";
 
 import { Item } from "../../types/item";
 import "./menu-item.styles.scss";
 
-const MenuItem = (props: { item: Item }) => {
+//setup to be able to use withRouter. This is required to get the histoty
+type RouterProps = {
+  title: string;
+};
+
+//add the specific properties here.
+interface MenuItemProps extends RouteComponentProps<RouterProps> {
+  item: Item;
+}
+
+const MenuItem = (props: MenuItemProps) => {
+  console.log(props.match);
   return (
-    <div className={`${props.item.size} menu-item`}>
-      <div className="background-image" style={{
-        backgroundImage: `url(${props.item.imageUrl})`,
-      }}></div>
+    <div
+      className={`${props.item.size} menu-item`}
+      onClick={() => {
+        console.log(props.match);
+        props.history.push(`${props.match.url}${props.item.linkUrl}`)}
+      }
+    >
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${props.item.imageUrl})`,
+        }}
+      ></div>
       <div className="content">
         <h1 className="title">{props.item.title.toUpperCase()}</h1>
         <span className="subtitle">SHOW NOW</span>
@@ -17,4 +38,4 @@ const MenuItem = (props: { item: Item }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
